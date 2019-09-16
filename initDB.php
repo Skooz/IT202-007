67509916f7d51807d570b7d5dab4786210ea703a
Skooz <gadzooks297@gmail.com>
2019-09-16 12:40:52 -0400

    updated initDB

--- initDB.php
+++ initDB.php
@@ -7,8 +7,7 @@ error_reporting(E_ALL);
 require('config.php');
 echo $host;
 
-$connection_string = "mysql:host=$host;dbname=$database;username=$username;
-password=$password;charset=utf8mb4";
+$connection_string = "mysql:host=$host;dbname=$database;username=$username;password=$password;charset=utf8mb4";
 
 try{
 	$db = new PDO($connection_string, $username, $password);
