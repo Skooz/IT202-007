a1bd1959837090c3d1a60034bfb2ff66199c613d
Skooz <gadzooks297@gmail.com>
2019-09-30 12:46:57 -0400

    homework finished 9/30

--- handleRequestData.php
+++ handleRequestData.php
@@ -1,19 +1,41 @@
 <?php
 	echo "<pre>" . var_dump($_GET, true) . "</pre>";
 
-	if(isset($_GET['name'])){
-		echo "<br>Hello, " . $_GET['name'] .  "<br>";
+	// Full name!
+	if(isset($_GET['firstname']) ){
+		echo "<br>Your first name is  " . $_GET['firstname'] .  "<br>";
 	}
-	if(isset($_GET['number'])){
-		echo "<br>" . $_GET['number'] . " should be a number...";
-		echo "<br>but it might not be<br>";
+	if(isset($_GET['lastname']) ){
+		echo "<br>Your last name is  " . $_GET['lastname'] . "<br>";
 	}
-	
-//TODO
-//handle addition of 2 or more parameters
-//concatenate 2 or more parameter values and echo them
-//Try passing multiple same-named parameters with different values
-//Try passing a parameter value with special characters
+
+	$fullName = $_GET['firstname'] . " " . $_GET['lastname'];
+	echo "<br>Your full name is  " . $fullName . "<br>";
+
+	// Add numbers!
+	if(isset($_GET['number1'])){
+		if(is_numeric($_GET['number1'])){
+			echo "<br>" . $_GET['number1'] . " is a number<br>";
+		}
+		else{
+			echo "<br>" . $_GET['number1'] . " is not a number<br>";
+		}
+	}
+	if(isset($_GET['number2'])){
+		if(is_numeric($_GET['number2'])){
+			echo "<br>" . $_GET['number2'] . " is a number<br>";
+		}
+		else{
+			echo "<br>" . $_GET['number2'] . " is not a number<br>";
+		}
+	}
+
+  if( isset($_GET['number2']) and isset($_GET['number1']) and is_numeric($_GET['number2']) and is_numeric($_GET['number1']) )
+  {
+	  $number3 = $_GET['number1'] + $_GET['number2'];
+	  echo "<br>number1 + number2 = " . $number3 . "<br>";
+   }
+		
 //handleRequestData.php?parameter1=a&p2=b	
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<html>
+<head>
+	<script>
+	//var myVar = 10;
+	//alert("My var is " + myVar);
+	console.log("Hello, Ed-boy!");
+	
+	//var name = prompt("What's your name?");
+	//alert("Hello, " + name);
+
+	var number = 0;
+	for(var i = 0; i < 10; i++){
+		number+=0.1;
+	}
+	console.log("Added float is " + number);
+	
+	var myParagraph = document.getElementById("myPara");
+	myParagraph.innerText = "Changed it";
+	let number1 = prompt("Pick a number");
+	let number2 = prompt("Pick another number");
+	myParagraph.innerText = number1 + " + " number2 + " = ";
+	myParagraph.innerText += (number1+number2);
+	console.log(myParagraph);
+	
+	//Google/Explore how to create an element and add it to the DOM
+	//create a div tag, add "added new element" as the text
+	//add it to the DOM body.
+
+	</script>
+</head>
+<body>
+	<p id="myPara">Just showing that we loaded something!!! (Bruh, periods are like, totally less enthusiastic, bruh.)</p>
+</body>
+</html>
+
 <?php
-echo "helo werld!!11! OwO"
+
+if(isset($_GET['name'])){
+	echo "Your GET name is " . $_GET['name'];
+}
 ?>
