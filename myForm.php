07ff623c325501565e2fa8931c010e5cf2d03b60
Skooz <gadzooks297@gmail.com>
2019-10-06 23:47:10 -0400

    created form for homework

---
+++ myForm.php
@@ -0,0 +1,37 @@
+<?php
+//helper
+function getName(){
+  if(isset($_GET['name'])){
+    echo "<p>Hello, " . $_GET['name'] . "</p>";
+  }
+}
+?>
+
+
+<html>
+  <head></head>
+  <body>
+  <?php getName();?>
+    <form mode="GET" action="#">
+    <input name="username" type="text" placeholder="Enter your username"/>
+    <input name="password" type="password" placeholder="Enter a password"/>
+    <input name="confirmpassword" type="password" placeholder="Confirm your password"/>
+    
+    <input type="submit" value="Log in"/>
+    
+    </form>
+  </body>
+</html>
+
+
+<?php
+$pass  = isset($_GET['password']);
+$cpass = isset($_GET['confirmpassword']);
+if( isset($_GET['password']) and isset($_GET['confirmpassword']) and $_GET['password'] == $_GET['confirmpassword'] ){
+  echo "<br><pre>" . var_export($_GET, true) . "</pre><br>";
+} elseif (isset($_GET['password']) and isset($_GET['confirmpassword']) and $_GET['password'] != $_GET['confirmpassword']){
+  echo "Passwords do not match. Try again.";
+} else {
+  echo "Please enter a username and password.";
+}
+?>
